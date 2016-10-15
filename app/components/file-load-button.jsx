ebf46cc8933b9f8ad3a2b6dff92ef335cf4d0a91
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-10-15 18:18:35 -0400

    Added file load button

---
+++ app/components/file-load-button.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+class FileLoadButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleFileChange(event) {
+        const files = event.target.files;
+        if(files && files.length > 0) {
+            this.props.fileChange(files[0]);
+        }
+    }
+
+    render() {
+        const btnClasses = "fa fa-upload fa-4x";
+        const btnStyles = {
+            position: "relative",
+            overflow: "hidden",
+            color: "#ff8b00"
+        };
+
+        const inputStyles = {
+            position: "absolute",
+            top: 0,
+            right: 0,
+            margin: 0,
+            padding: 0,
+            cursor: "pointer",
+            height: "100%"
+        };
+
+        return (
+            <div className={btnClasses} style={btnStyles}>
+                <input
+                    type="file"
+                    style={inputStyles}
+                    onChange={this.handleFileChange.bind(this)}
+                />
+            </div>
+        );
+    }
+}
+
+export default FileLoadButton;
