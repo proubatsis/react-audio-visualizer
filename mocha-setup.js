03a590b2059f4dfac9945d5eccb1a15cb359b8c6
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-10-15 18:33:35 -0400

    Added jsdom and setup mocha

---
+++ mocha-setup.js
@@ -0,0 +1,18 @@
+/* eslint quotes: 0, indent: 0 */
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
