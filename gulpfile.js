1f226e86460c6d84055073f92d9a26123414a22e
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-10-15 14:00:12 -0400

    Moved html to app folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp");
 var webpack = require("webpack-stream");
 
 gulp.task("webpack", function() {
-    return gulp.src("app/**/*.js")
+    return gulp.src("app/**/*")
         .pipe(webpack(require("./webpack.config.js")))
         .pipe(gulp.dest("output"));
 });
 
 gulp.task("html", function() {
-    return gulp.src("*.html").pipe(gulp.dest("output"));
+    return gulp.src("app/**/*.html").pipe(gulp.dest("output"));
 });
 
 gulp.task("default", ["html", "webpack"]);
