498e07dc80a75edaeb4691724c7252ebd4aea16f
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-10-16 11:06:39 -0400

    Added visualizer select button

---
+++ app/components/visualizer-button.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class VisualizerButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    clicked() {
+        this.props.visualizerSelect(this.props.visualizer);
+    }
+
+    render() {
+        const btnStyle = {
+            background: "#ff8b00",
+            color: "#fff",
+            cursor: "pointer",
+            padding: "0.25em"
+        };
+
+        return (
+            <div style={btnStyle} onClick={this.clicked.bind(this)}>
+                <p><i style={{paddingRight: "0.25em"}} className="fa fa-square"></i>{this.props.visualizerName}</p>
+            </div>
+        );
+    }
+}
+
+export default VisualizerButton;
---
+++ test/visualizer-button.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { assert, expect } from "chai";
+import { spy } from "sinon";
+
+import VisualizerButton from "../app/components/visualizer-button.jsx";
+import BarVisualizer from "../app/visualizers/bar-visualizer.js";
+
+describe("Visualizer button", () => {
+    it("should display given text", () => {
+        const button = shallow(
+            <VisualizerButton
+                visualizerName="Bar Visualizer"
+            />
+        );
+
+        expect(button.text()).to.equal("Bar Visualizer");
+    });
+
+    it("should call the visualizerSelect property when clicked", () => {
+        const cb = spy();
+        const button = shallow(
+            <VisualizerButton
+                visualizerName="Bar Visualizer"
+                visualizerSelect={cb}
+            />
+        );
+
+        expect(cb.callCount).to.equal(0);
+        button.simulate("click");
+        expect(cb.callCount).to.equal(1);
+    });
+
+    it("should pass the given visualizer to visualizerSelect property", () => {
+        const cb = spy();
+        const button = shallow(
+            <VisualizerButton
+                visualizerName="Bar Visualizer"
+                visualizer={BarVisualizer}
+                visualizerSelect={cb}
+            />
+        );
+
+        expect(cb.callCount).to.equal(0);
+        button.simulate("click");
+        expect(cb.callCount).to.equal(1);
+        assert(cb.calledWith(BarVisualizer));
+    });
+});
